908b24c3828becf3e00f518e187e5575a6e540b3
Neurochrom <pcichock@poczta.onet.pl>
2015-06-17 01:42:22 +0200

    Made the script use sips instead of ImageMagick convert, and made the start graphic source be given by a separate argument, while eliminating the baground color argument. Also updated the README.md file accordingly.

--- generator.sh
+++ generator.sh
@@ -2,9 +2,9 @@
 
 # Usage
 # -------------------------------------------
-usage() { echo "usage: $0 src_image bg_color [dst_dir]"; exit 1; }
+usage() { echo "usage: $0 icon_src_image start_src_image [out_dir]"; exit 1; }
 [ "$1" ] && [ "$2" ] || usage
-[ "$3" ] || set "$1" "$2" "./dst"
+[ "$3" ] || set "$1" "$2" "./out"
 
 
 # Directories
@@ -22,45 +22,44 @@ set -x
 
 # App Icons
 # -------------------------------------------
-c="convert -background none"
+c="sips"
 
 # iPhone iOS 5/6
-$c "$1" -resize 57x57   "$ICONDIR/Icon.png"
-$c "$1" -resize 114x114 "$ICONDIR/Icon@2x.png"
+$c "$1" --resampleHeightWidth 57 57   --out "$ICONDIR/Icon.png"
+$c "$1" --resampleHeightWidth 114 114 --out "$ICONDIR/Icon@2x.png"
 # iPad iOS 5/6
-$c "$1" -resize 72x72   "$ICONDIR/Icon-72.png"
-$c "$1" -resize 144x144 "$ICONDIR/Icon-72@2x.png"
+$c "$1" --resampleHeightWidth 72 72   --out "$ICONDIR/Icon-72.png"
+$c "$1" --resampleHeightWidth 144 144 --out "$ICONDIR/Icon-72@2x.png"
 # iPhone iOS7
-$c "$1" -resize 60x60   "$ICONDIR/Icon-60.png"
-$c "$1" -resize 120x120 "$ICONDIR/Icon-60@2x.png"
+$c "$1" --resampleHeightWidth 60 60   --out "$ICONDIR/Icon-60.png"
+$c "$1" --resampleHeightWidth 120 120 --out "$ICONDIR/Icon-60@2x.png"
 # iPad iOS7
-$c "$1" -resize 76x76   "$ICONDIR/Icon-76.png"
-$c "$1" -resize 152x152 "$ICONDIR/Icon-76@2x.png"
+$c "$1" --resampleHeightWidth 76 76   --out "$ICONDIR/Icon-76.png"
+$c "$1" --resampleHeightWidth 152 152 --out "$ICONDIR/Icon-76@2x.png"
 # Setting
-$c "$1" -resize 29x29   "$ICONDIR/Icon-Small.png"
-$c "$1" -resize 58x58   "$ICONDIR/Icon-Small@2x.png"
+$c "$1" --resampleHeightWidth 29 29   --out "$ICONDIR/Icon-Small.png"
+$c "$1" --resampleHeightWidth 58 58   --out "$ICONDIR/Icon-Small@2x.png"
 # Spotlight
-$c "$1" -resize 40x40   "$ICONDIR/Icon-Small-40.png"
-$c "$1" -resize 80x80   "$ICONDIR/Icon-Small-40@2x.png"
-$c "$1" -resize 50x50   "$ICONDIR/Icon-Small-50.png"
-$c "$1" -resize 100x100 "$ICONDIR/Icon-Small-50@2x.png"
+$c "$1" --resampleHeightWidth 40 40   --out "$ICONDIR/Icon-Small-40.png"
+$c "$1" --resampleHeightWidth 80 80   --out "$ICONDIR/Icon-Small-40@2x.png"
+$c "$1" --resampleHeightWidth 50 50   --out "$ICONDIR/Icon-Small-50.png"
+$c "$1" --resampleHeightWidth 100 100 --out "$ICONDIR/Icon-Small-50@2x.png"
 
 # AppStore
-$c "$1" -resize 512x512   "$ICONDIR/iTunesArtwork.png"
-$c "$1" -resize 1024x1024 "$ICONDIR/iTunesArtwork@2x.png"
+$c "$1" --resampleHeightWidth 512 512   --out "$ICONDIR/iTunesArtwork.png"
+$c "$1" --resampleHeightWidth 1024 1024 --out "$ICONDIR/iTunesArtwork@2x.png"
 
 
 # App Launch Images
 # -------------------------------------------
-c="convert $1 -background $2 -gravity center"
+c="sips"
 
 # iPhone
-$c -resize 256x256 -extent 320x480  "$SCREENDIR/Default~iphone.png"
-$c -resize 512x512 -extent 640x960  "$SCREENDIR/Default@2x~iphone.png"
-$c -resize 512x512 -extent 640x1136 "$SCREENDIR/Default-568h@2x~iphone.png"
+$c "$2" --resampleHeight 480  -c 480 320   --out "$SCREENDIR/Default~iphone.png"
+$c "$2" --resampleHeight 960  -c 960 640   --out "$SCREENDIR/Default@2x~iphone.png"
+$c "$2" --resampleWidth 640 -c 1136 640  --out "$SCREENDIR/Default-568h@2x~iphone.png"
 # iPad
-$c -resize 512x512   -extent 768x1024  "$SCREENDIR/Default-Portrait~ipad.png"
-$c -resize 1024x1024 -extent 1536x2048 "$SCREENDIR/Default-Portrait@2x~ipad.png"
-$c -resize 512x512   -extent 1024x768  "$SCREENDIR/Default-Landscape~ipad.png"
-$c -resize 1024x1024 -extent 2048x1536 "$SCREENDIR/Default-Landscape@2x~ipad.png"
-
+$c "$2" --resampleHeight 1024 -c 1024 768  --out "$SCREENDIR/Default-Portrait~ipad.png"
+$c "$2" --resampleHeight 2048 -c 2048 1536 --out "$SCREENDIR/Default-Portrait@2x~ipad.png"
+$c "$2" --resampleWidth 768   -c 1024 768  --out "$SCREENDIR/Default-Landscape~ipad.png"
+$c "$2" --resampleWidth 2048  -c 1536 2048 --out "$SCREENDIR/Default-Landscape@2x~ipad.png"
